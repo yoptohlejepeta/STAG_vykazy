87661a93ffd7e2f0722cb67bba1651aea565dcd9
yoptohlejepeta <peta.kotlan@seznam.cz>
2023-05-24 13:40:23 +0200

    sidebar refs, cached df

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ from io import StringIO, BytesIO
 import holidays
 from dotenv import load_dotenv
 
+from utils import get_df
+
 load_dotenv()
 
 
 st.set_page_config(layout="wide", page_title="STAG Výkazy", page_icon="📄")
+st.title("STAG Výkazy")
 
+
+# Dodatečné CSS
+# -----------------------------
 hide_table_row_index = """
             <style>
             thead tr th:first-child {display:none}
@@ -19,56 +25,89 @@ hide_table_row_index = """
             </style>
             """
 
+hover_css = """
+    <style>
+        a span:hover {
+            color: #FF4B4B; /* Replace with your desired hover color */
+        }
+    </style>
+    """
+
 st.markdown(hide_table_row_index, unsafe_allow_html=True)
+st.markdown(hover_css, unsafe_allow_html=True)
 
-st.title("STAG Výkazy")
+# Vlastní divider. st.divider kolem sebe dělá moc velké mezery
+st.markdown(
+    """
+    <style>
+    .custom-divider {
+        width: 100%;
+        height: 1px;
+        background-color: #bbb;
+        margin-top: -5px;
+        margin-bottom: -5px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+custom_divider = '<div class="custom-divider"></div>'
+# -----------------------------
 
+
+# Proměnné
+# -----------------------------
 rozvrh_url = "https://ws.ujep.cz/ws/services/rest2/rozvrhy/getRozvrhByUcitel"
 ucitel_url = "https://ws.ujep.cz/ws/services/rest2/ucitel/getUcitelInfo"
 czech_holidays = holidays.CZ(years=2023)
+vars = {
+    "vsechnyCasyKonani": True,
+    "jenRozvrhoveAkce": False,
+    "vsechnyAkce": True,
+    "jenBudouciAkce": False,
+    "lang": "cs",
+    "outputFormat": "CSV",
+    "rok": 2022,
+    "outputFormatEncoding": "utf-8",
+}
+mesice = {
+    "Leden": [datetime.date(2023, 1, 1), datetime.date(2023, 1, 31)],
+    "Únor": [datetime.date(2023, 2, 1), datetime.date(2023, 2, 28)],
+    "Březen": [datetime.date(2023, 3, 1), datetime.date(2023, 3, 31)],
+    "Duben": [datetime.date(2023, 4, 1), datetime.date(2023, 4, 30)],
+    "Květen": [datetime.date(2023, 5, 1), datetime.date(2023, 5, 31)],
+    "Červen": [datetime.date(2023, 6, 1), datetime.date(2023, 6, 30)],
+    "Červenec": [datetime.date(2023, 7, 1), datetime.date(2023, 7, 31)],
+    "Srpen": [datetime.date(2023, 8, 1), datetime.date(2023, 8, 31)],
+    "Září": [datetime.date(2022, 9, 1), datetime.date(2022, 9, 30)],
+    "Říjen": [datetime.date(2022, 10, 1), datetime.date(2022, 10, 31)],
+    "Listopad": [datetime.date(2022, 11, 1), datetime.date(2022, 11, 30)],
+    "Prosinec": [datetime.date(2022, 12, 1), datetime.date(2022, 12, 31)],
+}
+# -----------------------------
 
+# Výběr parametrů
+# -----------------------------
+# Ticket z headeru
 if "stagUserTicket" not in st.session_state:
     ticket = st.experimental_get_query_params().get("stagUserTicket")
     st.session_state["stagUserTicket"] = ticket
 
-
+# Pokud není tiket -> login stránka
 if not st.session_state["stagUserTicket"]:
     idnos = None
-    st.write(
-        f"""<h2>
-    Přihlašte se pomocí <a
-    href="{os.getenv("LOGIN_URL")}">Stagu</a></h2>""",
-        unsafe_allow_html=True,
-    )
+    login_url = os.getenv("LOGIN_URL")
+    st.header(f"Přihlašte se pomocí [Stagu]({login_url})")
 else:
     idnos = st.text_input(
         "Idno vyučujícího", placeholder="Zadejte Idno vyučujících oddělené čárkami"
     )
     col1, col2, col3 = st.columns(3)
-    typ = col1.radio("Časové období", ["Podle měsíce", "Datum od do"]) # "Podle semestru"
+    rozsah = col1.radio("Rozsah výkazů", ["Víkendy + svátky", "Všechny dny"])
+    typ = col2.radio("Časové období", ["Podle měsíce", "Datum od do"])
 
-    vars = {
-        "vsechnyCasyKonani": True,
-        "jenRozvrhoveAkce": False,
-        "vsechnyAkce": True,
-        "jenBudouciAkce": False,
-        "lang": "cs",
-        "outputFormat": "CSV",
-        "rok": 2022,
-        "outputFormatEncoding": "utf-8",
-    }
-
-    if typ == "Podle semestru":
-        semestr = col2.selectbox(
-            "Semestr",
-            ["", "ZS", "LS"],
-            help="Pokuď není specifikován, vrátí oba semestry.",
-        )
-        vars["semestr"] = semestr
-        vars.pop("datumOd", None)
-        vars.pop("datumDo", None)
-    elif typ == "Datum od do":
-        datum = col2.date_input(
+    if typ == "Datum od do":
+        datum = col3.date_input(
             label="Datum",
             value=[datetime.date(2022, 9, 19), datetime.date(2023, 9, 30)],
             min_value=datetime.date(2022, 9, 19),
@@ -81,142 +120,94 @@ else:
             pass
         vars.pop("semestr", None)
     elif typ == "Podle měsíce":
-        mesice = {
-            "Leden": [datetime.date(2023, 1, 1), datetime.date(2023, 1, 31)],
-            "Únor": [datetime.date(2023, 2, 1), datetime.date(2023, 2, 28)],
-            "Březen": [datetime.date(2023, 3, 1), datetime.date(2023, 3, 31)],
-            "Duben": [datetime.date(2023, 4, 1), datetime.date(2023, 4, 30)],
-            "Květen": [datetime.date(2023, 5, 1), datetime.date(2023, 5, 31)],
-            "Červen": [datetime.date(2023, 6, 1), datetime.date(2023, 6, 30)],
-            "Červenec": [datetime.date(2023, 7, 1), datetime.date(2023, 7, 31)],
-            "Srpen": [datetime.date(2023, 8, 1), datetime.date(2023, 8, 31)],
-            "Září": [datetime.date(2022, 9, 1), datetime.date(2022, 9, 30)],
-            "Říjen": [datetime.date(2022, 10, 1), datetime.date(2022, 10, 31)],
-            "Listopad": [datetime.date(2022, 11, 1), datetime.date(2022, 11, 30)],
-            "Prosinec": [datetime.date(2022, 12, 1), datetime.date(2022, 12, 31)],
-        }
-        mesic = col2.selectbox("Měsíc", mesice.keys())
+        mesic = col3.selectbox("Měsíc", mesice.keys())
         vars["datumOd"] = mesice.get(mesic)[0].strftime("%d/%m/%Y").replace("/", ".")
         vars["datumDo"] = mesice.get(mesic)[1].strftime("%d/%m/%Y").replace("/", ".")
-        vars.pop("semestr", None)
+
+# -----------------------------
 
 
 if idnos:
-    for idno in list(dict.fromkeys(idnos.replace(" ", "").split(","))): # ???
+    with st.sidebar:
+        st.markdown(
+            "<h2 style='font-size: 1.5em;'>Vyučující:</h2>", unsafe_allow_html=True
+        )
+
+    for idno in list(dict.fromkeys(idnos.replace(" ", "").split(","))):
         try:
-            vars["ucitIdno"] = int(idno)
+            idno = int(idno)
+            vars["ucitIdno"] = idno
         except:
             st.subheader(idno)
             st.write("Zkontrolujte, že jste správně zadali Idno vyučujícího.")
+            with st.sidebar:
+                st.markdown(
+                    f"<a href = #{idno} style='color: grey; text-decoration: none; font-size: 1.5em;'><span style='transition: color 0.3s;'>?{idno}?</span></a>",
+                    unsafe_allow_html=True,
+                )
+                st.markdown(custom_divider, unsafe_allow_html=True)
             continue
 
-        rozvrh = requests.get(
-            rozvrh_url,
-            cookies={"WSCOOKIE": st.session_state["stagUserTicket"][0]},
-            params=vars,
-        )
+        df, jmeno, prijmeni = get_df(idno, rozvrh_url, czech_holidays, vars, rozsah)
 
-        data = rozvrh.text
-
-        df = pd.read_csv(StringIO(data), sep=";")
-        if df.empty:
+        if jmeno == None:
             st.subheader(idno)
             st.write("Zkontrolujte, že jste správně zadali Idno vyučujícího.")
             continue
 
-        jmeno = df.loc[df.ucitIdno.astype(str) == idno]["jmeno.ucitel"].iloc[0]
-        prijmeni = df.loc[df.ucitIdno.astype(str) == idno]["prijmeni.ucitel"].iloc[0]
-
-        df.datum = pd.to_datetime(
-            df.datum.apply(lambda x: x.replace(".", "/")), format="%d/%m/%Y"
-        )
-        df.sort_values(by=["datum", "hodinaSkutOd"], ascending=True, inplace=True)
-        df.datum = df.datum.dt.strftime("%d/%m/%Y").apply(lambda x: x.replace("/", "."))
-
-        df = df.loc[
-            (df.denZkr == "So") | (df.denZkr == "Ne") & (~df.datum.isin(czech_holidays))
-        ]
-        df = df.loc[df["obsazeni"] > 0]
-
-        df.reset_index(inplace=True)
-
-        df["hodinaSkutOd"] = pd.to_datetime(df["hodinaSkutOd"], format="%H:%M")
-        df["hodinaSkutDo"] = pd.to_datetime(df["hodinaSkutDo"], format="%H:%M")
-        df["hodinaOdDo"] = (
-            df["hodinaSkutOd"]
-            .dt.strftime("%H:%M")
-            .str.cat(df["hodinaSkutDo"].dt.strftime("%H:%M"), sep="—")
-        )
-
-        df["kodPredmetu"] = df["katedra"].str.cat(df["predmet"], sep = "/")
-        df["pocetVyucHodin"] = df["pocetVyucHodin"].fillna(
-            (df["hodinaSkutDo"] - df["hodinaSkutOd"]).apply(lambda x: x.total_seconds())
-            / 3600
-        ).round().astype(int)
-
-        df["akce"] = df["kodPredmetu"].str.cat(
-            df["nazev"].str.cat(df["typAkceZkr"].apply(lambda x: f"({x})"), sep="  "),
-            sep="  ",
-        )
-        df.loc[df["kodPredmetu"] == df["nazev"], "akce"] = df["kodPredmetu"].str.cat(
-            df["typAkceZkr"].apply(lambda x: f"({x})"), sep="  "
-        )
-
+        # Tabulka
+        # -----------------------------
+        # Přejmenování sloupců kvůli výstupu
         df = df[["datum", "hodinaOdDo", "pocetVyucHodin", "akce"]]
-
-        try:
-            for i in range(len(df)):
-                row = df.iloc[i]
-                next_row = df.iloc[i + 1]
-                while (row["datum"] == next_row["datum"]) and (
-                    row["hodinaOdDo"] == next_row["hodinaOdDo"]
-                ):
-                    df.iloc[i, df.columns.get_loc("akce")] = " + ".join(
-                        [row["akce"], next_row["akce"]]
-                    )
-                    df.drop(labels=i + 1, inplace=True)
-                    df.reset_index(inplace=True, drop=True)
-                    next_row = df.iloc[i + 1]
-                    row = df.iloc[i]
-        except IndexError:
-            pass
-
         df.columns = ["Datum", "Hodina od do", "Počet hodin", "Akce"]
 
         hodiny_pocet = sum(df["Počet hodin"].fillna(0))
 
         if hodiny_pocet == 0:
-            # st.subheader(f":blue[{jmeno} {prijmeni}] ({idno})")
-            st.markdown(f"<h3> <text style= color:grey;>{jmeno} {prijmeni}</text> ({idno}) </h3>", unsafe_allow_html=True)
+            st.markdown(
+                f"<h3> <text style= color:grey;>{jmeno} {prijmeni}</text> ({idno}) <a id='{idno}'/></h3>",
+                unsafe_allow_html=True,
+            )
+            with st.sidebar:
+                st.markdown(
+                    f"<a href = #{idno} style='color: grey; text-decoration: none; font-size: 1.5em;'><span style='transition: color 0.3s;'><s>{jmeno} {prijmeni}</s> ({idno})</span></a>",
+                    unsafe_allow_html=True,
+                )
+                st.markdown(custom_divider, unsafe_allow_html=True)
         else:
-            st.subheader(f"{jmeno} {prijmeni} ({idno})")
+            st.subheader(f"{jmeno} {prijmeni} ({idno})", anchor=f"{idno}")
+            with st.sidebar:
+                st.markdown(
+                    f"<a href = #{idno} style='color: inherit; text-decoration: none; font-size: 1.5em;'><span style='transition: color 0.3s;'>{jmeno} {prijmeni} ({idno})</span></a>",
+                    unsafe_allow_html=True,
+                )
+                st.markdown(
+                    '<div class="custom-divider"></div>', unsafe_allow_html=True
+                )
 
-        # edited_df = st.experimental_data_editor(
-        #     df,
-        #     use_container_width=True,
-        #     height=((len(df)) + 2) * 35 + 3,
-        #     num_rows="dynamic",
-        # )
         st.table(df)
+        # -----------------------------
 
-        col1, col2 = st.columns([9, 1])
-
+        # Část pod tabulkou
+        # -----------------------------
+        col1, col2, col3 = st.columns([9, 1, 1])
         col1.metric("Celkem hodin", hodiny_pocet)
 
+        # Export dataframů
         buffer = BytesIO()
         with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
             df.to_excel(writer, sheet_name="Sheet1", index=False)
             writer.save()
 
             col2.download_button(
-                label="Stáhnout Excel",
+                label="📥 Excel",
                 data=buffer,
                 file_name=f"vykaz-{jmeno}-{prijmeni}-{idno}.xlsx",
                 mime="application/vnd.ms-excel",
             )
 
-        col2.download_button(
-            label="Stáhnout CSV",
+        col3.download_button(
+            label="📥 CSV",
             data=df.to_csv(index=False).encode("utf-8"),
             file_name=f"vykaz-{jmeno}-{prijmeni}-{idno}.csv",
             mime="text/csv",
---
+++ utils.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+import datetime
+from io import StringIO
+import datetime
+
+
+def get_month_days(year, month_name):
+    month_names_czech = {
+        "Leden": 1,
+        "Únor": 2,
+        "Březen": 3,
+        "Duben": 4,
+        "Květen": 5,
+        "Červen": 6,
+        "Červenec": 7,
+        "Srpen": 8,
+        "Září": 9,
+        "Říjen": 10,
+        "Listopad": 11,
+        "Prosinec": 12,
+    }
+
+    month = month_names_czech.get(month_name.lower())
+    if month is None:
+        return None, None
+
+    first_day = datetime.date(year, month, 1)
+    next_month = first_day.replace(day=28) + datetime.timedelta(days=4)
+    last_day = next_month - datetime.timedelta(days=next_month.day)
+
+    return first_day, last_day
+
+
+@st.cache_data
+def get_df(idno, url, holidays, vars, rozsah):
+    rozvrh = requests.get(
+        url,
+        cookies={"WSCOOKIE": st.session_state["stagUserTicket"][0]},
+        params=vars,
+    )
+
+    data = rozvrh.text
+    df = pd.read_csv(StringIO(data), sep=";")
+
+    if df.empty:
+        return df, None, None
+
+    jmeno = df.loc[df.ucitIdno == idno]["jmeno.ucitel"].iloc[0]
+    prijmeni = df.loc[df.ucitIdno == idno]["prijmeni.ucitel"].iloc[0]
+
+    try:
+        df.datum = pd.to_datetime(
+            df.datum.apply(lambda x: x.replace(".", "/")), format="%d/%m/%Y"
+        )
+    except:
+        pass
+    df.sort_values(by=["datum", "hodinaSkutOd"], ascending=True, inplace=True)
+    df.datum = df.datum.dt.strftime("%d/%m/%Y").apply(lambda x: x.replace("/", "."))
+
+    if rozsah == "Víkendy + svátky":
+        df = df.loc[
+            (df.denZkr == "So") | (df.denZkr == "Ne") & (~df.datum.isin(holidays))
+        ]
+    df = df.loc[df["obsazeni"] > 0]
+
+    df.reset_index(inplace=True)
+
+    df["hodinaSkutOd"] = pd.to_datetime(df["hodinaSkutOd"], format="%H:%M")
+    df["hodinaSkutDo"] = pd.to_datetime(df["hodinaSkutDo"], format="%H:%M")
+    df["hodinaOdDo"] = (
+        df["hodinaSkutOd"]
+        .dt.strftime("%H:%M")
+        .str.cat(df["hodinaSkutDo"].dt.strftime("%H:%M"), sep="—")
+    )
+
+    df["kodPredmetu"] = df["katedra"].str.cat(df["predmet"].astype("str"), sep="/")
+    df["pocetVyucHodin"] = (
+        df["pocetVyucHodin"]
+        .fillna(
+            (df["hodinaSkutDo"] - df["hodinaSkutOd"]).apply(lambda x: x.total_seconds())
+            / 3600
+        )
+        .round()
+        .astype(int)
+    )
+
+    df["akce"] = df["kodPredmetu"].str.cat(
+        df["nazev"].str.cat(df["typAkceZkr"].apply(lambda x: f"({x})"), sep="  "),
+        sep="  ",
+    )
+    df.loc[df["kodPredmetu"] == df["nazev"], "akce"] = df["kodPredmetu"].str.cat(
+        df["typAkceZkr"].apply(lambda x: f"({x})"), sep="  "
+    )
+
+    try:
+        for i in range(len(df)):
+            row = df.iloc[i]
+            next_row = df.iloc[i + 1]
+            while (row["datum"] == next_row["datum"]) and (
+                row["hodinaOdDo"] == next_row["hodinaOdDo"]
+            ):
+                df.iloc[i, df.columns.get_loc("akce")] = " + ".join(
+                    [row["akce"], next_row["akce"]]
+                )
+                df.drop(labels=i + 1, inplace=True)
+                df.reset_index(inplace=True, drop=True)
+                next_row = df.iloc[i + 1]
+                row = df.iloc[i]
+    except IndexError:
+        pass
+
+    return df, jmeno, prijmeni
